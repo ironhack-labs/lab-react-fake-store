055e7b22f694c51f884a1513f3066c7272afd020
VladysslavTkach <tkachacc.program@gmail.com>
2024-02-05 14:21:26 +0100

    done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,25 +1,37 @@
-import { useState } from "react";
-
-
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ItemCard from '../components/ItemCard';
 function ProductDetailsPage() {
-  // The state variable `product` is currently an empty object {},
-  // but you should use it to store the response from the Fake Store API (the product details).
-  const [product, setProduct] = useState({});
+	// The state variable `product` is currently an empty object {},
+	// but you should use it to store the response from the Fake Store API (the product details).
+	const [product, setProduct] = useState({});
+	const { productId } = useParams();
+	useEffect(() => {
+		fetch(`https://fakestoreapi.com/products/${productId}`)
+			.then((res) => res.json())
+			.then((json) => setProduct(json));
+      console.log(`in`);
+	}, []);
+  console.log(product);
+	// The `productId` coming from the URL parameter is available in the URL path.
+	// You can access it with the `useParams` hook from react-router-dom.
 
+	// To fetch the product details, set up an effect with the `useEffect` hook:
 
-  // The `productId` coming from the URL parameter is available in the URL path.
-  // You can access it with the `useParams` hook from react-router-dom.
-
-
-  // To fetch the product details, set up an effect with the `useEffect` hook:
-
-
-
-  return (
-    <div className="ProductDetailsPage">
-    {/* Render product details here */}
-    </div>
-  );
+	return (
+		<div className="ProductDetailsPage">
+			{/* <ItemCard
+				id={product.id}
+				title={product.title}
+				category={product.category}
+				description={product.description}
+				image={product.image}
+				price={product.price}
+        ratingCount={product.rating.count}
+        ratingRate={product.rating.rate}
+			/> */}
+		</div>
+	);
 }
 
 export default ProductDetailsPage;
