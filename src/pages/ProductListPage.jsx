01b88bd908b7cc24486356d7ff3b2ae2721ab0b7
Stefan Kummerlöw <gummiz@gmx.net>
2024-02-12 15:39:49 +0100

    done without bonus

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,44 +1,43 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "../styles/ProductListPage.css"
+import "../styles/ProductListPage.css";
+import { Link } from "react-router-dom";
 
 function ProductListPage() {
-  // The state variable `products` is currently an empty array [], 
+  // The state variable `products` is currently an empty array [],
   // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
-  const baseUrl = "https://fakestoreapi.com/products"
+  const baseUrl = "https://fakestoreapi.com/products";
 
-
-  useEffect(()=>{
-    axios.get(baseUrl)
-    .then(response => {
-      setProducts(response.data)
-      
-    })
-    .catch( err => console.log(err))
-  },[])
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="ProductListPage">
-      
-      {
-        products.map((productObj) => {
-          return (
-
-          <div className="ProductListPage__product" key={productObj.id}>
-            <img src={productObj.image} alt="" />
-            <div className="title">{productObj.title}</div>
-            <div className="category">{productObj.category}</div>
-            <div className="price">{productObj.price}</div>
-            <div className="description">{productObj.description.slice(0,30)}</div>
-          </div>
-          )
-        })
-      }
-
-
+      {products.map((productObj) => {
+        console.log("ID:", productObj.id);
+        return (
+          <Link to={`./product/details/${productObj.id}`} key={productObj.id}>
+            <div className="ProductListPage__product" >
+              <img src={productObj.image} alt="" />
+              <div className="title">{productObj.title}</div>
+              <div className="category">{productObj.category}</div>
+              <div className="price">${productObj.price}</div>
+              <div className="description">
+                {productObj.description.slice(0, 30)}
+              </div>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
