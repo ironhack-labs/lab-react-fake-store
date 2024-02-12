4f25280e7af863a5e8e4ca65008f2f38cec9b752
Laia Navalon <laianavalon@gmail.com>
2024-02-12 19:50:11 +0100

    Iteration 3

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link, useParams } from 'react-router-dom';
+import axios from "axios";
 
 
 function ProductDetailsPage() {
@@ -9,15 +11,36 @@ function ProductDetailsPage() {
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
-
+  const { productId } = useParams()
 
   // To fetch the product details, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    axios.get(`https://fakestoreapi.com/products/${productId}`)
+      .then((response) => {
+        setProduct(response.data)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }, [])
 
-
+  console.log(product)
 
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+      {/* Render product details here */}
+      {product !== null &&
+        <div className="productDetails card">
+          <img src={product.image} alt={product.title} className="productDetailsImg" />
+          <div className="category">{product.category}</div>
+          <div className="title detailsPage">{product.title}</div>
+          <div className="productInfo">
+            <div className="description">{product.description}</div>
+            <div className="price">${product.price}</div>
+          </div>
+          <hr />
+          <Link><button class="back-btn">Back</button></Link>
+        </div>}
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -19,25 +19,24 @@ function ProductListPage() {
       })
   }, [])
 
-  console.log(products)
-
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
 
-      {products === null
-        ? <div>Loading...</div>
-        : products.map((elm) => {
+      {products !== null &&
+        products.map((elm) => {
           return (
+            <div className="ProductListPage" key={elm.id}>
             <Link to={`/product/details/${elm.id}`}>
-              <div key={elm.id} className="listPage">
+              <div className="listPage">
                 <img src={elm.image} alt={elm.title} className="productImg" />
                 <div className="title">{elm.title}</div>
-                <div className="category">{elm.category}</div>
-                <div className="price">${elm.price}</div>
-                <div className="description">{elm.description.slice(0, 50)}</div>
+                <div>{elm.category}</div>
+                <div>${elm.price}</div>
+                <div>{elm.description.slice(0, 50)}</div>
               </div>
             </Link>
+            </div>
           )
         })
 
