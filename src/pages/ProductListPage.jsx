261d120d1a3cba7e475fafb0163c3784d77c60ea
Laia Navalon <laianavalon@gmail.com>
2024-02-12 15:41:22 +0100

    Iteration 1-2 done

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 
 function ProductListPage() {
@@ -7,11 +9,39 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setProducts(response.data)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }, [])
 
+  console.log(products)
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+
+      {products === null
+        ? <div>Loading...</div>
+        : products.map((elm) => {
+          return (
+            <Link to={`/product/details/${elm.id}`}>
+              <div key={elm.id} className="listPage">
+                <img src={elm.image} alt={elm.title} className="productImg" />
+                <div className="title">{elm.title}</div>
+                <div className="category">{elm.category}</div>
+                <div className="price">${elm.price}</div>
+                <div className="description">{elm.description.slice(0, 50)}</div>
+              </div>
+            </Link>
+          )
+        })
+
+      }
     </div>
   );
 }
