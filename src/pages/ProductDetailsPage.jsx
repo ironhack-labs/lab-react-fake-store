b3337d654f83e097c67adc805c9b5b210d265ba7
Tushar16-1998 <tusharsunder2@gmail.com>
2024-02-13 21:36:59 +0000

    done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
-
+import { useEffect , useState } from "react";
+// import ProductListPage from "./ProductListPage";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
-  const [product, setProduct] = useState({});
+  
+  const [product, setProduct] = useState([]);
+  const { productId } = useParams();
+  
+  useEffect(() => {
+
+    axios
+    .get(`https://fakestoreapi.com/products/${productId}`)
+    .then((response) => {
+      setProduct(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  } , [productId])
 
 
   // The `productId` coming from the URL parameter is available in the URL path.
@@ -17,7 +33,16 @@ function ProductDetailsPage() {
 
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+    {product && (
+      <article key={product.id}>
+        <span><img className="productImg" src={product.image} /></span>
+        <span className="productCategory">{product.category}</span>
+        <span className="productTitle">{product.title}</span>
+        <span className="productPrice">{product.price}$</span>
+        <span className="productDec">{product.description}</span>
+        <button className="backbutton" onClick={() => window.history.back()}>Back</button>
+      </article>
+    )}
     </div>
   );
 }
