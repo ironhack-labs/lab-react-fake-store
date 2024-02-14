31aae9cfa1a770828daec9bc8b164988fac7f46f
piyush <piyushbishthld123@gmail.com>
2024-02-14 10:15:07 +0100

    lab-react-fake-store

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import { Card, CardBody, CardFooter, Stack, Heading, Text, Button } from '@chakra-ui/react'
 
 
 function ProductListPage() {
@@ -7,11 +11,51 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
-
+useEffect(() => {
+axios.get("https://fakestoreapi.com/products")
+.then((response) => {
+  console.log(response)
+  setProducts(response.data)
+})
+.catch((error) => {
+  console.log(error)
+})
+}, [])
 
   return (
     <div className="ProductListPage">
-      {/* Render list of products here */}
+      {
+        products && products.map((product) => {
+          return (
+            <Card
+  direction={{ base: 'column', sm: 'row' }}
+  overflow='hidden'
+  variant='outline'
+  key={product.id}
+>
+  <img height="100px" width="200px" src={product.image}></img>
+  
+
+  <Stack>
+    <CardBody display="flex" alignItems="center">
+      <Heading size='md'>{product.title}</Heading>
+
+      <Text py='2'>{product.category}</Text>
+      <Text py='2'>{product.price}</Text>
+      <Text py='2'>{product.description}</Text>
+    </CardBody>
+    <CardFooter>
+      <Link to={`/products/details/${product.id}`}>
+    <Button colorScheme="teal">Details</Button>
+    </Link>
+    </CardFooter>
+   
+
+  </Stack>
+</Card>
+          )
+        })
+      }
     </div>
   );
 }
