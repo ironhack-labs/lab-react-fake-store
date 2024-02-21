411e9235911867f238c153e41961400f7e112ef7
Adri Priego <priegolucena@gmail.com>
2024-02-21 03:08:36 +0100

    Store

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Axios } from "axios";
 
 
 function ProductListPage() {
@@ -7,13 +9,28 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products")
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
 
 
   return (
     <div className="ProductListPage">
-      {/* Render list of products here */}
+      <h2>Categoria: </h2>
+      {products.map((product, index) => (
+        <li key={index}>
+          <h3>{product.data[0]}</h3>
+        </li>
+      ))}
     </div>
   );
 }
+
 
 export default ProductListPage;
