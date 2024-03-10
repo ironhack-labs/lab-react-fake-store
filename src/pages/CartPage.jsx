d34979eb81d816f586f90717f145c3cf33e79886
Kumar Daryanani <destral@msn.com>
2024-03-10 21:17:13 +0100

    Done, minus bonus iterations

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,29 +3,69 @@ import { useParams, Link } from "react-router-dom";
 
 function CartPage() {
     const [cart, setCart]=useState({});
-    const [products, setProducts]=useState([]);
+    const [cartProducts, setCartProducts]=useState([])
+    const [productData, setProductData]=useState([]);
     
-    const cartID = useParams().cartId
+    const {cartId} = useParams();
+
+    function fetchCartData(cartId) {
+        fetch(`https://fakestoreapi.com/carts/${cartId}`)
+        .then(response => response.json())
+        .then(jsonData => {
+            setCart(jsonData);
+            setCartProducts(jsonData.products);
+            console.log(cart.products);
+            fetchProductData();
+        })
+        .catch(error => console.error(error));
+
+
+    }
+    const fetchProductData = ()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then(jsonData => setProductData(jsonData))
+        .catch(error => console.error(error));
+    }
 
     useEffect(()=>{
-        fetch(`https://fakestoreapi.com/carts/${cartID}`)
-        .then(response => response.json())
-        .then(jsonData => setCart(jsonData))
-        .catch(error => console.error(error));
-        console.log(cart);
-        fetch('https://fakestoreapi.com/products')
-        .then(jsonData => setProducts(jsonData))
-        .catch(error => console.error(error));
-        console.log(products)
+        fetchCartData(cartId);
       }, [])
 
     return(
         <>
-            <p>{cartID}</p>
+            <p>{cartId}</p>
             <ul>
-            {cart[cartID][products].map((cartItem)=>{
-            <li><span>{cart.products.productId}</span> <span>{cart.products.quantity}</span></li>
+                {cart.products && cart.products.map((cartItem)=>{
+                    /*
+                    const product = productData.find((product) => product.id === cartItem.productId)
+                    console.log(product);
+                    */
+                    return
+                        {/*
+                        <div  className='product-card' key='cartItem.productId'>
+                            <img src="{product.image}" alt="{product.title}" />
+                            <p className = 'product-title'>{product.title}</p>
+                            <p>{product.category}</p>
+                            <p>{'$'+product.price.toFixed(2)}</p>
+                            <p className = 'product-description'>{product.description}</p>
+                            
+                        </div>
+                         */}
+                    )
+                })}
+                {/*}
+                {cart.products.map((cartItem)=>{
+            return(
+                  <div className="product-card">
+                    <img src={cartItem.image} alt={cartItem.title} />
+                    <p className="product-title">{cartItem.title}</p>
+                    <p>{cartItem.category}</p>
+                    <p>{'$'+cartItem.price.toFixed(2)}</p>
+                    <p className="product-description">{cartItem.description}</p>
+                  </div>
+              )
             })}
+        */}
             </ul>
         </>
        )
