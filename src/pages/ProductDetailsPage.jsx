277f12d844d04d659cf2ead5c2a864913687e1f1
Aliakbar Torbati <alitorbati1368@gmail.com>
2024-03-11 18:33:56 +0100

    version 0

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,23 +1,56 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import cartImg from "../assets/cart.png";
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
-  const [product, setProduct] = useState({});
-
-
-  // The `productId` coming from the URL parameter is available in the URL path.
-  // You can access it with the `useParams` hook from react-router-dom.
-
+  const [product, setProduct] = useState(null);
+  const { productId } = useParams();
+  console.log("productId", productId);
 
   // To fetch the product details, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    const getDetails = async () => {
+      try {
+        const detailResponse = await axios(
+          `https://fakestoreapi.com/products/${productId}`
+        );
+        console.log("details", detailResponse.data);
+        setProduct(detailResponse.data);
+      } catch (error) {
+        console.log("Error from details", error);
+      }
+    };
+    getDetails();
+
+    console.log("every product", product);
+  }, [productId]); //  <-- The effect will run every time the `productId`` changes
 
 
+  const backToHome = () =>{
+    window.location.href = "/";
+  }
+  if (!product) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+      <div>
+        <img src={product.image} alt="image of the product" />
+        <h3>{product.category}</h3>
+        <h3>{product.title}</h3>
+        <div>
+        <h3>{product.description}</h3>
+        <h3>{product.price}</h3>
+        </div>
+        <button className="backBtn" onClick={backToHome}>back</button>
+        {/* <Link to={`/carts/${product.id}`}>
+        <img src={cartImg} alt="Cart Image" />
+        </Link> */}
+      </div>
     </div>
   );
 }
