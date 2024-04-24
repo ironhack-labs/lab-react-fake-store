4a566d8a7f14e0e17bf1350126c55228cf67b29c
camilaalbuq <cam.albuquer@gmail.com>
2024-04-24 13:23:22 +0200

    lab solved, does not include bonus cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+//building a simple React application that displays a list of products
+//and allows you to view the details of each product.
+// using the Fake Store API to get the data that you will show in the app. This API provides endpoints that return
+// fake data for an online store, such as a list of products, product details, and a shopping cart
+
+//GET https://fakestoreapi.com/products -> returns a json response with lists of all products
+//GET https://fakestoreapi.com/products/:id -> returns a JSON response with details of a specific product by ID
+
 import "./App.css";
 import Navbar from "./components/Navbar";
 import ProductListPage from "./pages/ProductListPage";
@@ -5,18 +13,20 @@ import ProductDetailsPage from "./pages/ProductDetailsPage";
 
 import { Routes, Route } from "react-router-dom";
 
-
 function App() {
-
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
 
       <Routes>
         <Route path="/" element={<ProductListPage />} />
-        <Route path="/product/details/:productId" element={<ProductDetailsPage />} />
+        <Route
+          path="/product/details/:productId"
+          element={<ProductDetailsPage />}
+          // iteration 02
+          // we need this to route to each product, it was already pre-set
+        />
       </Routes>
-
     </div>
   );
 }
