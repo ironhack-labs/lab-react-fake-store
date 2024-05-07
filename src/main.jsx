6745a315058a4c5433b96a966e0526e492053a39
Christian Kuechler <ckuechler@gmail.com>
2024-05-07 20:19:28 +0200

    Lab done

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,17 +1,44 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function ProductListPage() {
-  // The state variable `products` is currently an empty array [], 
+  // The state variable `products` is currently an empty array [],
   // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
-
   // To fetch the list of products, set up an effect with the `useEffect` hook:
 
-
+  useEffect(() => {
+    const baseURL = "https://fakestoreapi.com/products";
+    axios
+      .get(baseURL)
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((e) => {
+        console.log("error getting list of characters...", e);
+      });
+  }, []);
   return (
     <div className="ProductListPage">
-      {/* Render list of products here */}
+      {
+        /* Render list of products here */
+        products?.map((product) => {
+          return (
+            <div key={product.id}>
+            <Link to={`product/details/${product.id}`}>
+              <div className="card">
+                <img src={product.image} alt="" />
+                <p>{product.title}</p>
+                <p>{product.category}</p>
+                <p>${product.price}</p>
+                <p>{product.description}</p>
+              </div>
+            </Link>
+            </div>
+          );
+        })
+      }
     </div>
   );
 }
