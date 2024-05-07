3d49b342f08174016f42dc29502fd4cd5bc3c643
Asier MD <asier.md09@gmail.com>
2024-05-07 14:36:29 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProductListPage from "./pages/ProductListPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 
 import { Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<ProductListPage />} />
+        <Route path="/cart/:cartId" element={<Cart />} />
         <Route path="/product/details/:productId" element={<ProductDetailsPage />} />
       </Routes>
 
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,23 +1,47 @@
-import { useState } from "react";
+import { useState } from "react"
+import { useEffect} from "react"
+import { useParams,Link } from "react-router-dom"
 
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
+  const params = useParams()
   const [product, setProduct] = useState({});
-
+  
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
 
-
   // To fetch the product details, set up an effect with the `useEffect` hook:
 
+  useEffect(()=>{
+    fetch(`https://fakestoreapi.com/products/${params.productId}`)
+    .then((response)=>{
+      return response.json()
+    })
+    .then((response)=>{
+      setProduct(response)      
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+
+
+  },[params])
 
 
   return (
     <div className="ProductDetailsPage">
     {/* Render product details here */}
+      <div className="card text-center">
+        <img src={product.image} style={{width:"150px",border:"solid 1px gray"}} alt="img" />
+        <p style={{fontWeight:"bold"}}>{product.title} -</p>
+        <p>{product.category}</p>
+        <p>{product.price}</p>
+        <p>{product.description}</p>
+      </div>
+    <Link to={"/"}><button>Go back</button></Link>
     </div>
   );
 }
