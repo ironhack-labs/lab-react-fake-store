dcb60933da6b25e5163284c3bb5432186861fafe
Greg <gajdukow@protonmail.com>
2024-05-08 08:57:31 +0200

    lab

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 
 function ProductListPage() {
@@ -6,12 +7,27 @@ function ProductListPage() {
   // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
 
+  let dataLoaded = false;
+
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products")
+    .then((response) => {
+      setProducts(response.data)
+      dataLoaded = true;
+      console.log("data was loaded")
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
 
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+      {dataLoaded === true && products[0].title}
+
     </div>
   );
 }
