348c93838e96501d77e749008df1b5c72862781a
Claire Lacanal <lacanal.claire@gmail.com>
2024-05-12 00:03:15 +0200

    lab solved

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,23 +1,47 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
-  // but you should use it to store the response from the Fake Store API (the product details).
   const [product, setProduct] = useState({});
 
+  // but you should use it to store the response from the Fake Store API (the product details).
 
   // The `productId` coming from the URL parameter is available in the URL path.
+  const { productId } = useParams();
   // You can access it with the `useParams` hook from react-router-dom.
 
-
   // To fetch the product details, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://fakestoreapi.com/products/${productId}`
+      );
+      console.log(response);
+      const data = await response.json();
+      setProduct(data);
+    }
+    fetchData();
+  }, [productId]);
 
-
-
+  const { image, title, price, category, description } = product;
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+      <div className="detail">
+        <img src={image} alt="" />
+        <hgroup className="detail-item">
+          <p className="cat">{category}</p>
+          <h1>{title}</h1>
+          <div className="flex-detail">
+            <p className="description">{description}</p>
+            <p className="price">{price} $</p>
+          </div>
+          <Link to="/" className="back-button">
+            Back
+          </Link>
+        </hgroup>
+      </div>
     </div>
   );
 }
