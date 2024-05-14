a82bab1112b91bb4efe4997bcb66f331f0956104
Orlando Balzer <ojbalzer@gmail.com>
2024-05-14 15:07:54 +0200

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import logo from "./../assets/logo-ironhack-blue.png";
 import cart from "./../assets/cart.png";
 import { Link } from "react-router-dom";
+import profile from "../assets/profile-icon.png";
 
 function Navbar() {
   return (
@@ -20,13 +21,13 @@ function Navbar() {
 
         <div className="w-1/4 flex justify-end mr-4">
           {/* User Profile Button */}
-            <button className="flex items-center text-l py-1">
-              <img
-                src={cart}
-                alt="Cart icon"
-                className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
-              />
-            </button>
+          <button className="flex items-center text-l py-1">
+            <img
+              src={cart}
+              alt="Cart icon"
+              className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
+            />
+          </button>
         </div>
       </div>
     </nav>
