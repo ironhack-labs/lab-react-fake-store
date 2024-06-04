014865e97e96e6c93f2528ada0f3eaf14bd181d8
Filipa <filipamnmlopes@hotmail.com>
2024-06-04 14:52:51 +0200

    iteration 2 done already

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
   const [product, setProduct] = useState({});
+  const {productId} = useParams()
 
 
   // The `productId` coming from the URL parameter is available in the URL path.
