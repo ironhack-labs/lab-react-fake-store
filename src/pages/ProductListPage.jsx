cc228d1f33f0b153b570548512d61b213d306e48
Thomas Kruithof <tkruithof95@gmail.com>
2024-07-02 14:02:16 +0200

    iteration 4

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 
 
 function ProductListPage() {
@@ -7,11 +10,43 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products")
+    .then((response) => {
+
+      if (!response.status === 200) {
+        throw new Error("Server response not ok");
+      }
+
+      const data = response.data;
+      console.log(data);
+
+      setProducts(data);
+
+    })
+    .catch((error) => {
+      console.log("error: ", error);
+    });
+  }, []);
 
 
   return (
     <div className="ProductListPage">
-      {/* Render list of products here */}
+      
+      <ul>
+        {products.map(product => (
+          <Link to={`/product/details/${product.id}`} key={product.id}>
+            <li className="listItem card flex-center">
+                <img src={product.image} className="productImg"/>
+                <p className="productTitle">{product.title}</p>
+                <p>{product.category}</p>
+                <p>€{product.price}</p>
+                <p>{product.description}</p>
+            </li>
+          </Link>
+        ))}
+      </ul>
+
     </div>
   );
 }
