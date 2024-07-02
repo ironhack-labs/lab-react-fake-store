7b329312d642ac7ea671dbc3569d82323c0b9c92
Thomas Kruithof <tkruithof95@gmail.com>
2024-07-02 22:08:22 +0200

    solved lab

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,47 +5,78 @@ import axios from "axios";
 const CartPage = () => {
 
     const [products, setProducts] = useState([])
+    const [productDetails, setProductDetails] = useState([])
 
     useEffect(() => {
-        axios.get("https://fakestoreapi.com/carts/1")
-        .then((response) => {
-    
+      const fetchCart = async() => {
+        try {
+          const response = await axios.get(`https://fakestoreapi.com/carts/5`);
+          
           if (response.status !== 200) {
             throw new Error("Server response not ok");
           }
-    
+
           const data = response.data;
-          console.log(data);
+          console.log("Cart data: ", data);
     
           setProducts(data.products);
-    
-        })
-        .catch((error) => {
-          console.log("error: ", error);
-        });
-      }, []);
+
+        } catch (error) {
+          console.log("Error: ", error);
+        }
+      }
+
+      fetchCart()
+      
+    }, []);
 
 
       useEffect(() => {
-        const productsArr = products.map(product => {
-            axios.get(`https://fakestoreapi.com/products/${product.productId}`)
-        });
-        
-        Promise.all(productsArr).then((response) => {
-            
-        })
-        
 
+        const fetchDetails = async() => {
+          try {
+            const productsArr = products.map(product => (
+              axios.get(`https://fakestoreapi.com/products/${product.productId}`)
+            ));
+
+            const responses = await Promise.all(productsArr);
+
+            responses.forEach(response => {
+              if (response.status !== 200) {
+                throw new Error("Server response not ok");
+              }
+            });
+
+            const data = responses.map(response => response.data);
+
+            console.log("product data: ", data);
+
+            setProductDetails(data)
+
+          } catch (error) {
+            console.log("error: ", error)
+          }
+        }
+
+        if (products.length) {
+          fetchDetails();
+        }
       }, [products] )
+
+
 
     return (
         <div className="cartPage">
             <h1>Shopping cart</h1>
             <ul>
-                {products.map(product => (
-                <Link to={`/product/details/${product.productId}`} key={product.productId}>
+                {productDetails.map(product => (
+                <Link to={`/product/details/${product.id}`} key={product.id}>
                     <li className="listItem card flex-center">
-                        product
+                    <img src={product.image} className="productImg"/>
+                    <p className="productTitle">{product.title}</p>
+                    <p>{product.category}</p>
+                    <p>€{product.price}</p>
+                    <p className="productDescription">{product.description}</p>
                     </li>
                 </Link>
                 ))}
