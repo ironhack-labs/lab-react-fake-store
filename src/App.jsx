25e8462ce711fc26782f5eab621eb3131ff5c43e
Carlos Carvalho <CarlosCarvalho1995@hotmail.com>
2024-08-08 21:22:56 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import './pages/ProductListPage.css';
 import Navbar from "./components/Navbar";
 import ProductListPage from "./pages/ProductListPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
