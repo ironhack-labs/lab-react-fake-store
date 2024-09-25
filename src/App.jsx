10d138b9dffb5f2e28d5966b614b3eabebfc258f
Javier Gascon <javitocatral@gmail.com>
2024-09-25 19:45:16 +0200

    lab terminado menos el ultimo bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import ProductListPage from "./pages/ProductListPage";
-import ProductDetailsPage from "./pages/ProductDetailsPage";
+import './App.css'
+import Navbar from './components/Navbar'
+import ProductListPage from './pages/ProductListPage'
+import ProductDetailsPage from './pages/ProductDetailsPage'
 
-import { Routes, Route } from "react-router-dom";
-
+import { Routes, Route } from 'react-router-dom'
+import CartPage from './pages/CartPage'
 
 function App() {
-
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
 
       <Routes>
         <Route path="/" element={<ProductListPage />} />
-        <Route path="/product/details/:productId" element={<ProductDetailsPage />} />
+        <Route
+          path="/product/details/:productId"
+          element={<ProductDetailsPage />}
+        />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
-
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,25 +1,52 @@
-import { useState } from "react";
-
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 function ProductDetailsPage() {
-  // The state variable `product` is currently an empty object {},
-  // but you should use it to store the response from the Fake Store API (the product details).
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState({})
+  const { productId } = useParams()
 
+  useEffect(() => {
+    getData()
+  }, [productId]) // Add productId to the dependency array
 
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        `https://fakestoreapi.com/products/${productId}`
+      )
+      const result = await response.json()
+      setProduct(result)
+    } catch (error) {
+      console.log('Error fetching product details', error)
+    }
+  }
+
+  if (product === undefined) {
+    return <h3>no funciona</h3>
+  }
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
 
-
   // To fetch the product details, set up an effect with the `useEffect` hook:
-
-
 
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+      <div className="cardDetail">
+        <div>
+          <img style={{ width: '250px' }} src={product.image} alt="" />
+        </div>
+        <h4>
+          <strong>{product.title}</strong>
+        </h4>
+        <p> {product.category}</p>
+        <p>{product.price}€</p>
+        <p className="descripcion">{product.description}</p>
+        <Link to={'/'}>
+          <button>Back</button>
+        </Link>
+      </div>
     </div>
-  );
+  )
 }
 
-export default ProductDetailsPage;
+export default ProductDetailsPage
