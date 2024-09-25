b7d0723226c83a9ee599305dd6896afccc1be3f3
unknown <maludietrich.00@gmail.com>
2024-09-25 16:52:04 +0200

    done

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import ProductDetailsPage from "./ProductDetailsPage";
 
 
 function ProductListPage() {
@@ -7,11 +9,40 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setProducts(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
 
+  if (products === undefined) {
+    return <p>...</p>
+  }
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+      {products.map((eachProduct) => {
+        return (
+          <ul key={eachProduct.id}>
+            <Link to={`/product/details/${eachProduct.id}`}>
+              <li>
+                <img style={{ width: '20vh' }} src={eachProduct.image} alt=''></img>
+                <p>{eachProduct.title}</p>
+                <p>{eachProduct.category}</p>
+                <p>{eachProduct.price}</p>
+                <p>{eachProduct.description}</p>
+              </li>
+            </Link>
+          </ul>
+        )
+      })}
     </div>
   );
 }
