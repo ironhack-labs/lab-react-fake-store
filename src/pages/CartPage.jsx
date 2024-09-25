863f5fcc70968d8dcbe00a6c554e0cd67c496eee
Miguel Angel <migueel26@gmail.com>
2024-09-25 22:36:57 +0200

    lab hecho

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,22 +3,17 @@ import { Link, Navigate } from "react-router-dom";
 
 function CartPage() {
 
-    //Save los productos del Cart 
     const [cartProducts, setCartProducts] = useState([]);
 
-    //Which product to fetch
     const [productId, setProductId] = useState(-1);
 
-    //Save each Product Details
     const [productsDetail, setProductsDetail] = useState([]);
 
-    //ComponentDidMount => Solo al crearse
     useEffect(()=>{
         getData();
     }
     ,[]);
 
-    //Cuando actualicemos productId => Ya se ha guardado un producto nuevo
     useEffect(()=>{
         console.log("productId modificado", cartProducts.length );
         if(cartProducts.length > 0){
@@ -27,7 +22,6 @@ function CartPage() {
         }
     }, [productId])
 
-    //Obtenemos los detales del cart y obtenemos el primer producto
     async function getData() {
         try {
             const response = await fetch(`https://fakestoreapi.com/carts/5`);
@@ -37,23 +31,22 @@ function CartPage() {
 
         } catch (error) {
             console.log(error);
-            // navigate("/error")
         }
     }
 
-    //Obtenemos los detalles de un nuevo producto y pasamos al siguiente
+    
     async function getProductData(id) {
         try {
             
             const response = await fetch(`https://fakestoreapi.com/products/${id}`);
             const data = await response.json();
 
-            //Añadimos un producto nuevo que renderizar
+            
             let cloneProducts = structuredClone(productsDetail);
             cloneProducts = [...cloneProducts, data];
             setProductsDetail(cloneProducts);
 
-            //Update de la lista de productos
+            
             console.log(cartProducts);
             let cloneCart = structuredClone(cartProducts);
             cloneCart.shift();
@@ -68,14 +61,14 @@ function CartPage() {
     }
 
     if(productId === -1) return <h3>Loading page...</h3>
-    // if(!Object.keys(cartProducts).length ) return <h3>Loading page...</h3>
+    
 
     return(
         <> 
         
             {productsDetail.map((product)=>{
                 return(
-                    // <h1 key={product.id}>{product.id}</h1>
+                    
                     <Link key={product.id} to={`/product/details/${product.id}`} >
                         <div className="card flex-center">
                             <img className="img-100" src={product.image} alt="product image" />
