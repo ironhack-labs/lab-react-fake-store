bc01cb9f193cc4c30657a706cf23456d67b67b6f
Miguel Angel <migueel26@gmail.com>
2024-09-25 22:25:32 +0200

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,10 @@ function App() {
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
-
       <Routes>
         <Route path="/" element={<ProductListPage />} />
         <Route path="/product/details/:productId" element={<ProductDetailsPage />} />
       </Routes>
-
     </div>
   );
 }
