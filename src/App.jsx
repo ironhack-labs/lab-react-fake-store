e2b9636981b378ab84c81ca53f279e3c99cfa69a
Miguel Angel <migueel26@gmail.com>
2024-09-25 22:28:44 +0200

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProductListPage from "./pages/ProductListPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 
 import { Routes, Route } from "react-router-dom";
+import CartPage from "./pages/CartPage";
 
 
 function App() {
@@ -11,10 +12,13 @@ function App() {
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
+
       <Routes>
         <Route path="/" element={<ProductListPage />} />
         <Route path="/product/details/:productId" element={<ProductDetailsPage />} />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
+
     </div>
   );
 }
