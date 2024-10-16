be5c5c8e3840f0bab1efa25fb8e48c12744e4b04
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-10-16 16:37:59 +0200

    Done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {  } from "react";
+import { Link, useParams } from "react-router-dom";
 
 
 function ProductDetailsPage() {
+  const {productId} = useParams()
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
-  const [product, setProduct] = useState({});
+  const [productItem, setProductItem] = useState({
+  });
+  const productUrl = `https://fakestoreapi.com/products/${productId}`
+
+  const getProductData = async(urlName) =>{
+    try{
+      const response = await fetch(urlName);
+      const data = await response.json();
+      setProductItem(data);
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  
+
+
+  useEffect(()=>{
+    getProductData(productUrl)
+  }, {})
 
 
   // The `productId` coming from the URL parameter is available in the URL path.
@@ -16,8 +39,16 @@ function ProductDetailsPage() {
 
 
   return (
-    <div className="ProductDetailsPage">
-    {/* Render product details here */}
+    <div className="product-details-page">
+    {<>
+      <img className="product-details-image" src={productItem.image} />
+      <p className="product-details-title">{productItem.title}</p>
+      <p className="product-details-category">{productItem.category }</p>
+      <p>${productItem.price}</p>
+      <p className="product-details-description">{productItem.description}</p>
+      <Link to="/"><button style={{ background: "green", color: "white" }}>Back</button></Link>
+      </>
+    }
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,17 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 function ProductListPage() {
   // The state variable `products` is currently an empty array [], 
   // but you should use it to store the response from the Fake Store API (the list of products).
+  const url = "https://fakestoreapi.com/products";
   const [products, setProducts] = useState([]);
+  // const [error, setError] = useState(null);
 
+  const getData = async(urlName) =>{
+    try{
+      const response = await fetch(urlName);
+      const data = await response.json();
+      setProducts(data);
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+ 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
-
-
+  useEffect(()=>{
+    getData(url);
+   }, [])
+  
+   console.log(products)
   return (
     <div className="ProductListPage">
-      {/* Render list of products here */}
+
+      {products.map((product)=>{
+
+       return <Link key={product.id} to={`/product/details/${product.id}`}>
+        <>
+        <div className="product-list-container">
+          <img className="list-container-image" src={product.image} />
+          <p className="list-container-title">{product.title}</p>
+          <p className="list-container-category">{product.category}</p>
+          <p>${product.price}</p>
+          <p className="list-container-description">{product.description}</p>
+        </div>
+        </>
+       </Link>
+      })}
     </div>
   );
 }
