9a03c9c9c8dfa07b744a97d79bd32bb1d3fe8154
Sebastian Luft <sebastian.luft.gm@gmail.com>
2024-10-16 17:53:20 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import ProductDetailsPage from "./pages/ProductDetailsPage";
 
 import { Routes, Route } from "react-router-dom";
 
-
 function App() {
-
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
 
       <Routes>
         <Route path="/" element={<ProductListPage />} />
-        <Route path="/product/details/:productId" element={<ProductDetailsPage />} />
+        <Route
+          path="/product/details/:productId"
+          element={<ProductDetailsPage />}
+        />
       </Routes>
-
     </div>
   );
 }
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,23 +1,72 @@
-import { useState } from "react";
-
-
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
   const [product, setProduct] = useState({});
-
-
+  const { productId } = useParams();
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
 
-
   // To fetch the product details, set up an effect with the `useEffect` hook:
-
-
-
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${productId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Update the state variable `product` with the response from the API
+        setProduct(data);
+      });
+  }, [productId]);
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "start",
+          padding: "20px 40px",
+          background: "white",
+        }}
+      >
+        {product && (
+          <>
+            <img src={product.image} style={{ maxWidth: "300px" }} />
+            <h3
+              style={{
+                background: "blue",
+                color: "white",
+              }}
+            >
+              {product.category}
+            </h3>
+            <h1
+              style={{
+                fontWeight: "bold",
+                maxWidth: "500px",
+                flexWrap: "wrap",
+              }}
+            >
+              {product.title}
+            </h1>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "20px",
+                maxWidth: "700px",
+              }}
+            >
+              <p>{product.description}</p>
+              <h1 style={{ color: "blue" }}>${product.price}</h1>
+            </div>
+          </>
+        )}
+      </div>
+      <Link to="/">
+        <button style={{ background: "green", color: "white" }}>Back</button>
+      </Link>
     </div>
   );
 }
