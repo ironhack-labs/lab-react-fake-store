2574a767c70c857b6c63b8718c6ef83215fc6b94
Vicente Duch <vhduch@gmail.com>
2024-10-29 19:18:18 +0100

    lab done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
   const [product, setProduct] = useState({});
+  let { productId } = useParams()
+
+  useEffect(() => {
+   
+    axios.get(`https://fakestoreapi.com/products/${productId}`)
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching product details:", error);
+      });
+  }, [productId]);
 
 
   // The `productId` coming from the URL parameter is available in the URL path.
@@ -17,7 +30,10 @@ function ProductDetailsPage() {
 
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+      {product.image && <img src={product.image} alt={product.title} />}
+      <h3>{product.title}</h3>
+      <h3>Price: ${product.price}</h3>
+      <h3>Description: {product.description}</h3>
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,17 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 
 function ProductListPage() {
   // The state variable `products` is currently an empty array [], 
   // but you should use it to store the response from the Fake Store API (the list of products).
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(null);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
-
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setProducts(response.data)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  })
 
   return (
-    <div className="ProductListPage">
-      {/* Render list of products here */}
+    <div className="container">
+      {products && products.map((productDetails, index) => {
+        return (
+          <div key={index}>
+            <Link to={`/product/details/${productDetails.id}`}>
+              <img  src={productDetails.image} />
+              <h1>{productDetails.title}</h1>
+              <h3>{productDetails.price}</h3>
+              <h3>{productDetails.description}</h3>
+            </Link>
+
+          </div>
+        )
+      })}
     </div>
   );
 }
