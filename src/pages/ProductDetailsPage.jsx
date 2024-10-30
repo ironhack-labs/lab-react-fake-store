929920e01a2e80fcd38ea74bb0a1a226f3517e1f
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-10-30 08:14:24 +0100

    fake shop lab olga w5d2

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,23 +1,36 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function ProductDetailsPage() {
-  // The state variable `product` is currently an empty object {},
-  // but you should use it to store the response from the Fake Store API (the product details).
   const [product, setProduct] = useState({});
 
+  const { productId } = useParams();
 
-  // The `productId` coming from the URL parameter is available in the URL path.
-  // You can access it with the `useParams` hook from react-router-dom.
-
-
-  // To fetch the product details, set up an effect with the `useEffect` hook:
-
-
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .catch((e) => {
+        console.log("Error getting character details from the API...");
+        console.log(e);
+      });
+  }, []);
 
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+      {product ? (
+        <>
+          <img src={product.image} alt={product.title} />
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+        </>
+      ) : (
+        <p>Loading product details...</p>
+      )}
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,16 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function ProductListPage() {
-  // The state variable `products` is currently an empty array [],
-  // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
   useEffect(() => {
     axios.get("https://fakestoreapi.com/products").then((response) => {
       setProducts(response.data);
     });
   }, []);
-  console.log(products);
 
   return (
     <div className="ProductListPage">
@@ -19,6 +18,7 @@ function ProductListPage() {
           <img src={productObj.image} />
           <p>{productObj.title}</p>
           <p>${productObj.price}</p>
+          <Link to={`/product/details/${productObj.id}`}>More Details</Link>
         </div>
       ))}
     </div>
