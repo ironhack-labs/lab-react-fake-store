921781391c95d1fb79da73089925d03038643fba
Hernâni Silva <hcsilvamatos@hotmail.com>
2024-11-15 22:29:51 +0000

    Solved Lab

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 function ProductDetailsPage() {
@@ -9,15 +10,24 @@ function ProductDetailsPage() {
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
-
+const {productId} = useParams()
 
   // To fetch the product details, set up an effect with the `useEffect` hook:
-
+useEffect(() => {
+  fetch(`https://fakestoreapi.com/products/${productId}`)
+  .then(response => response.json())
+  .then(data => setProduct(data))
+}, [productId])
 
 
   return (
     <div className="ProductDetailsPage">
     {/* Render product details here */}
+    <img src={product.image} alt="" />
+    <h1>{product.title}</h1>
+    <p>{product.category}</p>
+    <p>{product.description}</p>
+    <p>{product.price} €</p>
     </div>
   );
 }
