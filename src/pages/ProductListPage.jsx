a24fa1516e885df111b2e2b5a5c9802e4d80e503
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-27 19:41:36 +0100

    Iteration 1 solved

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 
@@ -7,11 +8,31 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+        .then((response) => response.json())
+        .then((productsJson) => setProducts(productsJson))
+        .catch((error) => console.log(error))
+  }, [])
 
+console.log(products);
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+      <ul>
+        {products.map((product, index )=> {
+          return(
+          <>
+            <li key={index+'.'+product.id}><img src={product.image} alt={product.title} /></li>
+            <li key={index+'.'+product.id}>Title: {product.title}</li>
+            <li key={index+'.'+product.id}>Description: {product.description}</li>
+            <li key={index+'.'+product.id}>Price: {product.price}</li>
+            <li key={index+'.'+product.id}>Rating: {product.rating.rate}</li>
+          </>
+          )
+        })}
+      </ul>
     </div>
   );
 }
