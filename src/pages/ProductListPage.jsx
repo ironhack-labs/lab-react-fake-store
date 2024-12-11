015933d42bc53f2b1ddfd7701cce3df73832cbc3
Nacho Monzón <ignaciomonzona@gmail.com>
2024-12-11 18:36:36 +0100

    Iteration 3 complete

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -14,25 +14,31 @@ function ProductListPage() {
   return (
     <div className="ProductListPage">
       <h1 className="font-bold mb-4">Products</h1>
-      <ul className="grid grid-cols-1 gap-4">
-        {products.map((product) => (
-          <li
-            key={product.id}
-            className="flex-row space-around p-4 border rounded gap-2"
-          >
-            <Link to={`/product/details/${product.id}`} className="w-full">
-              <img
-                src={product.image}
-                className="w-full h-48 object-contain mb-4"
-              />
-              <p className="text-lg font-bold">{product.title}</p>
-              <p className="text-lg">{product.category}</p>
-              <p className="text-sm text-gray-700">${product.price}</p>
-              <p className="text-sm text-gray-700">{product.description}</p>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <ul className="grid-cols-1 gap-4">
+  {products.map((product) => (
+    <li
+      key={product.id}
+      className="border p-4 rounded-md shadow-sm hover:shadow-lg transition-all"
+    >
+      <Link to={`/product/details/${product.id}`} className="block">
+        <div className="flex flex-row space-y-5 d-flex align-items-baseline">
+          <img
+          src={product.image}
+          alt={product.title}
+          className="w-48 h-48 object-contain mb-4"
+        />
+          <h2 className="text-lg font-bold">{product.title}</h2>
+          <p className="text-sm text-gray-600">{product.category}</p>
+          <p className="text-sm text-gray-700">${product.price}</p>
+          <p className="text-sm text-gray-500 truncate">
+            {product.description}
+          </p>
+        </div>
+      </Link>
+    </li>
+  ))}
+</ul>
+
     </div>
   );
 }
