164df36e691ac293d54e49b9a9aac474ed402af7
ayat <abuhajayat@gmail.com>
2025-01-21 13:01:41 +0100

    done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+//import axios from "axios";
 
 
 function ProductDetailsPage() {
+  const{ productId } = useParams();
+  console.log('productId', productId );
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
   const [product, setProduct] = useState({});
 
-
+ useEffect(()=> {
+  fetch(`https://fakestoreapi.com/products/${productId}`)
+   .then(Response => Response.json())
+   .then(data => setProduct(data))
+   .catch(err => console.log(err))
+ }, [])
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
 
@@ -17,8 +26,12 @@ function ProductDetailsPage() {
 
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
-    </div>
+    <img src={product.image} width={'100px'} height={'100px'} />
+      <h1>{product.title}</h1>
+      <p>{product.category}</p>
+      <p>${product.price}</p>
+      <p>{product.description}</p>
+      </div>
   );
 }
 
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,19 +1,41 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import './ProductListPage.css'
+import { Link } from "react-router-dom";
 
 function ProductListPage() {
   // The state variable `products` is currently an empty array [], 
   // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
 
-  // To fetch the list of products, set up an effect with the `useEffect` hook:
+useEffect(()=> {
+  fetch('https://fakestoreapi.com/products')
+  .then(Response => Response.json())
+  .then(data => setProducts(data))
+  .catch(err => console.log(err))
+}, [])
 
+if(products.length === 0){
+  return <p>Loading...</p>
+}   
 
-  return (
-    <div className="ProductListPage">
-      {/* Render list of products here */}
-    </div>
-  );
+return (
+  <div className="ProductListPage">
+    {products.map(product => (
+      <div key={product.id} className="ProductCard">
+        {/* Link to the ProductDetailsPage with the productId */}
+        <Link to={`/product/details/${product.id}`} className="ProductLink">
+          <img src={product.image} alt={product.title} width={'100px'} height={'100px'} />
+          <div className="ProductDetails">
+            <h1>{product.title}</h1>
+            <p>{product.category}</p>
+            <p>${product.price}</p>
+            <p>{product.description}</p>
+          </div>
+        </Link>
+      </div>
+    ))}
+  </div>
+);
 }
 
 export default ProductListPage;
