5e74119768bbb75fef2923f7bfa6ffad21f32aca
Paulo Prado <paulroar@hotmail.com>
2025-01-22 16:01:26 +0000

    Lab done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import ProductListPage from "./pages/ProductListPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
-
 import { Routes, Route } from "react-router-dom";
 
 
@@ -11,7 +10,6 @@ function App() {
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
-
       <Routes>
         <Route path="/" element={<ProductListPage />} />
         <Route path="/product/details/:productId" element={<ProductDetailsPage />} />
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,24 +1,52 @@
-import { useState } from "react";
-
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
-  const [product, setProduct] = useState({});
+  const { productId } = useParams();
+  const [product, setProduct] = useState(null);
+  const navigate = useNavigate();
 
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
 
+  useEffect(() => {
+    async function fetchProduct() {
+      console.log("Fetching product with ID:", productId)
+      try {
+        const response = await axios.get(`https://fakestoreapi.com/products/${productId}`);
+        setProduct(response.data);
+      } catch (error) {
+        console.error("Error", error);
+      }
+    }
+
+    fetchProduct();
+  }, [productId]);
+
+  if (!product) {
+    return <p>Loading product details...</p>;
+  }
 
   // To fetch the product details, set up an effect with the `useEffect` hook:
 
-
-
   return (
-    <div className="ProductDetailsPage">
+    <div className="card">
     {/* Render product details here */}
+
+    <h1>{product.title}</h1>
+      <img src={product.image} alt={product.title} style={{ width: "200px" }} />
+      <p><strong>Description:</strong> {product.description}</p>
+      <p><strong>Price:</strong> ${product.price}</p>
+      <p><strong>Category:</strong> {product.category}</p>
+            <button onClick={() => navigate("/")} className="btn-primary" >Go back</button>
+
     </div>
+
+    
   );
 }
 
