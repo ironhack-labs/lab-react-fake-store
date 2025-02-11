b7aec0360ebe9ddcd8315c6195a96581ddcebe02
Dvdrepetto <davidmrepetto@gmail.com>
2025-02-11 19:54:34 +0200

    done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 
 function ProductDetailsPage() {
@@ -9,15 +11,35 @@ function ProductDetailsPage() {
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
-
+const {productId} = useParams()
 
   // To fetch the product details, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${productId}`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json); 
+        setProduct(json); 
+      })
+      .catch((error) => {
+        console.error("Error fetching product details:", error);
+      });
+  }, [productId]);
 
-
+  if (!product.title) {
+    return <p>Loading product details...</p>;
+  }
 
   return (
-    <div className="ProductDetailsPage">
-    {/* Render product details here */}
+    <div className="ProductListPage">
+      <div className="product-list">
+      <h1>{product.title}</h1>
+      <img src={product.image} alt={product.title} />
+      <p className="product-category">{product.category}</p>
+        <p className="product-price">${product.price}</p>
+        <p className="product-title">{product.title}</p>
+        <button><Link to={"/"}>Back</Link></button>
+      </div>
     </div>
   );
 }
