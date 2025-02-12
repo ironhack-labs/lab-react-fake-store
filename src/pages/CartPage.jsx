2bc552bf5a23534d01dfbf97963a76c7ffd46748
nestorsdelgado <nestorsdelgado@gmail.com>
2025-02-12 14:47:46 +0000

    Lab solution - Néstor

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,16 +1,61 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
 
 export default function CartPage() {
 
-    const { productId } = useParams()
-    const navigate = useNavigate();
-    const [products, setProducts] = useState({});
-    
+    const getCart = 'https://fakestoreapi.com/carts/5'
+
+    const [cart, setCart] = useState({});
+
+    useEffect(() => {
+
+        fetch(getCart)
+            .then(response => response.json())
+            .then(data => setCart(data))
+            .catch((err) => console.log(err));
+    }, [])
+
+    const getSpecificProducts = cart?.products?.map((product, index) => (
+        <>
+            <div className="product-card" key={index}>
+                <p> https://fakestoreapi.com/products/{product.productId}</p>
+                <p>{product.productId} </p>
+            </div>
+        </>
+    ));
+
+
+    const getProducts = 'https://fakestoreapi.com/products'
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+        fetch(getProducts)
+            .then(response => response.json())
+            .then(data => setProducts(data))
+            .catch((err) => console.log(err));
+    }, [])
+
+    const cartProductIds = cart?.products?.map(product => product.productId) || [];
+    const filteredProducts = products.filter(product => cartProductIds.includes(product.id));
+
     return (
         <>
             <div>
-                <h1>Cart</h1>
+                <div className="ProductListPage" key={cart.id}>
+                    {filteredProducts.length > 0 ? (
+                        filteredProducts.map(product => (
+                            <div className="product-card" key={product.id}>
+                                <img src={product.image} className="product-img" alt={product.title} />
+                                <p className="product-name">{product.title}</p>
+                                <p className="product-info">{product.category}</p>
+                                <p className="product-info">${product.price}</p>
+                                <p className="product-desc">{product.description}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <p>Loading cart products...</p>
+                    )}
+                </div>
             </div>
         </>
     )
