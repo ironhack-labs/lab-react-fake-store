cf831d2b59db97c9ff1425fe9a778407902da8a3
Johana Castellano <johanacg16@gmail.com>
2025-02-12 10:13:57 +0000

    bonus getting info works

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -11,13 +11,15 @@ export default function CartPage() {
   // The state variable `products` is currently an empty array [],
   const [products, setProducts] = useState([]);
 
+  let productsIds = [];
+
   // To fetch the cart details, set up an effect with the `useEffect` hook:
   useEffect(() => {
     /* console.log("useEffect hook - fetching cart"); */
     axios.get(singleCartURL).then((response) => {
         setCart(response.data);
         /* console.log(response.data.products) */
-        const productsIds = response.data.products.map(product => product.productId); //me quedo con las ids de los 
+        productsIds = response.data.products.map(product => product.productId); //me quedo con las ids de los 
         //productos que tiene cart, para luego filtrar los productos del array que devolverá la request al products endpoint 
         return axios.get(allProductsURL); /* --> aquí la segunda request*/
     })
@@ -28,6 +30,8 @@ export default function CartPage() {
             return productsIds.includes(product.id)
         }) //no utilizo operadores de comparación porque quiero comparar varios, sino quizás podría hacer un map con los operadores de comparación
 
+        console.log(filterCartProducts);
+        
         setProducts(filterCartProducts);
     })
     .catch((error) => { //Establece un catch por si ocurre algún error al traer la información
