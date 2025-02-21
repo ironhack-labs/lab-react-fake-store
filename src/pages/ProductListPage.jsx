fc04f390ecdb8157ce597f44c8a948029848c4cd
sashaknw <kaniewa.aleksandra@gmail.com>
2025-02-21 15:41:25 +0000

    done
    sin bonus

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,19 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-
+const URLProduct = "https://fakestoreapi.com/products";
 function ProductListPage() {
-  // The state variable `products` is currently an empty array [], 
-  // but you should use it to store the response from the Fake Store API (the list of products).
+  const [ fetching, setFetching ] = useState(true);
   const [products, setProducts] = useState([]);
 
-  // To fetch the list of products, set up an effect with the `useEffect` hook:
+function fetchProducts() {
+   axios
+     .get(URLProduct)
+     .then((response) => {
+       console.log(response);
+       setProducts(response.data);
+     })
+     .catch((error) => {
+       console.log(error);
+       setFetching(false);
+     });
+  
+}
+    useEffect(() => {
+    fetchProducts();
+  }, []);
 
+ 
+  if (products.length === 0) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className="ProductListPage">
-      {/* Render list of products here */}
+   
+     <div className="ProductListPage">
+    
+     {products.map((product) => (
+        <div key={product.id} className="card">
+          <img src={product.image} alt={product.title} className="product-image" />
+          <div className="product-details">
+            <h2 className="text-lg font-semibold">{product.title}</h2>
+            <p className="text-gray-600">Price: ${product.price}</p>
+            <p className="text-gray-600">Description: {product.description}</p>
+
+            <Link to={`/product/details/${product.id}`}>
+              <button className="btn-primary">View Details</button>
+            </Link>
+    
+          </div>
+        </div>
+      ))}
     </div>
   );
+   
 }
 
 export default ProductListPage;
