eb89b4fec5ad21670fa0c76d076ebce19ed03874
Juan Marin <jmo129@gmail.com>
2025-03-04 16:34:46 +0100

    done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 function ProductDetailsPage() {
@@ -10,14 +11,35 @@ function ProductDetailsPage() {
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
 
+  const parametrosDinamicos = useParams();
+  console.log(parametrosDinamicos);
 
   // To fetch the product details, set up an effect with the `useEffect` hook:
 
 
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${parametrosDinamicos.productId}`)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      //console.log(data)
+      setProduct(data);
+    })
+    .catch((error)=> {
+      console.log(error)
+    })
+
+  },[])
 
   return (
     <div className="ProductDetailsPage">
     {/* Render product details here */}
+    <img src={product.image} alt="foto-producto" />
+    <p>{product.category}</p>
+    <h2>{product.title}</h2>
+    <p>{product.price}</p>
+    <p>{product.description}</p>
     </div>
   );
 }
