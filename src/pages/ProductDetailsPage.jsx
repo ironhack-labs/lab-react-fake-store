cf1d8273fda7cf14ffc6a648fc2e37f4a8da77e1
Samuel Perez Besada <samusitesi@gmail.com>
2025-03-04 16:15:15 +0100

    Done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
   const [product, setProduct] = useState({});
 
+  const parametrosDinamicos = useParams()
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
@@ -13,11 +14,29 @@ function ProductDetailsPage() {
 
   // To fetch the product details, set up an effect with the `useEffect` hook:
 
-
+  useEffect(()=>{
+    fetch(`https://fakestoreapi.com/products/${parametrosDinamicos.productId}`)
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        setProduct(data)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}, [parametrosDinamicos.productId])
 
   return (
-    <div className="ProductDetailsPage">
+    <div className="ProductDetailsPage" style={{display:"flex", flexDirection:"column", alignItems:"center", padding:"10px"}}>
     {/* Render product details here */}
+      <img src={product.image} width="350px" style={{ border: "1px solid #000"}}/>
+      <h2 style={{width:"250px"}}><strong>{product.title}</strong></h2>
+      <div style={{display:"flex", flexDirection:"row", alignItems:"center", marginTop:"15px"}}>
+      <h3 style={{backgroundColor:"blue", color:"white", padding:"5px", borderRadius:"10px", marginRight:"10px"}}>{product.category}</h3>
+      <p style={{backgroundColor:"green", color:"white", padding:"5px", borderRadius:"10px"}}>${product.price}</p>
+      </div>
+      <p style={{display:"inline-block", justifyContent:"center", width:"400px", marginTop:"30px"}}>{product.description}</p>
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 function ProductListPage() {
@@ -7,11 +8,37 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
-
+  useEffect(()=>{
+    fetch("https://fakestoreapi.com/products")
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      setProducts(data)
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }, [])
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+      
+      {products.map((eachProduct)=>{
+        
+        return(
+          <Link key={eachProduct.id} to={`/product/details/${eachProduct.id}`} style={{ margin:"20px"}}>
+            <div style={{display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"space-between",  border: "1px solid #000", padding:"10px"}}>
+            <img src={eachProduct.image} width="150px" style={{ border: "1px solid #000"}}/>
+            <h2 style={{width:"250px"}}><strong>{eachProduct.title}</strong></h2>
+            <h3>{eachProduct.category}</h3>
+            <p>${eachProduct.price}</p>
+            <p style={{display:"inline-block", justifyContent:"center", width:"250px"}}>{eachProduct.description}</p>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   );
 }
