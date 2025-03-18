7000e08428a081c972702e9e36e42216b4a8d2c3
Christian Wirsching <christianwirsching@gmail.com>
2025-03-18 16:14:57 +0100

    done

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+export const Cart = () => {
+    const [cart, setCart] = useState(null)
+    const cartId = 6
+    
+    useEffect(()=>{
+        fetch(`https://fakestoreapi.com/carts/${cartId}`)
+        .then(response => response.json())
+        .then(data => setCart(data)) // do the mapping directly in here. Do curly braces. 
+        .catch((err)=>console.log(err))
+    },[cartId])
+
+    useEffect(()=>{
+        console.log(cart)
+    }, [cart])
+
+    useEffect(()=>{
+    if (cart.product !== null){
+    const ProductIdArray = cart.products.map((product)=>product.id);
+    console.log(ProductIdArray)
+    }},[cart])
+    
+
+
+    // useEffect(()=>{
+    //     fetch(`https://fakestoreapi.com/products/${cart.id}`)
+    //     .then(response => response.json())
+    //     .then(data => setProduct(data))
+    //     .catch((err)=>console.log(err))
+    //     },[productId])
+
+
+
+
+  return (
+    <div></div>
+  )
+}
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,23 +1,31 @@
+import { use, useEffect } from "react";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
   const [product, setProduct] = useState({});
-
-
-  // The `productId` coming from the URL parameter is available in the URL path.
+    // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
-
-
+  const { productId } = useParams();
+  console.log(productId);
   // To fetch the product details, set up an effect with the `useEffect` hook:
+  useEffect(()=>{
+    fetch(`https://fakestoreapi.com/products/${productId}`)
+  .then(response => response.json())
+  .then(data => setProduct(data))
+  .catch((err)=>console.log(err))
+  },[productId])
 
-
-
+console.log(product)
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+      <img src= {product.image} alt="product image" />
+      <h1>{product.title}</h1>
+      <div>{product.category}</div>
+      <div>{product.description}</div>
     </div>
   );
 }
