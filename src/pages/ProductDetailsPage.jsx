d99f741c4d153ff53e9d8b0126a57e409ab27304
Sosha Fedotovskaia <sinterma@gmail.com>
2025-03-18 16:03:32 +0100

    done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
-
+import { useState, useEffect  } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
@@ -9,17 +10,48 @@ function ProductDetailsPage() {
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
+  let params = useParams()
 
+// To fetch the product details, set up an effect with the `useEffect` hook:
+useEffect(() => {
+    
+async function getProduct(){
+  try{
+  
+    const response = await fetch(`https://fakestoreapi.com/products/${params.productId}`)
+    const json = await response.json();
+    setProduct(json)
+  }
+  catch(error){
+    console.error(error.message);
+  }
+}
+  getProduct(params.productId)
+},[params.productId]
 
-  // To fetch the product details, set up an effect with the `useEffect` hook:
+)
+// console.log(product);
+  
 
 
 
   return (
+    <>
     <div className="ProductDetailsPage">
     {/* Render product details here */}
+    <img src = {product.image}  alt="psroduct_image" />
+    <span className="category">{product.category}</span>
+     <h2>{product.title} </h2>
+      <div className="descriptionPrice">
+        <p>{product.description}</p>
+        <span className="price">${product.price}</span>
+      </div>
+
     </div>
+   <Link to={'/'}> <button className="back">Back</button></Link>
+   </>
   );
 }
+
 
 export default ProductDetailsPage;
