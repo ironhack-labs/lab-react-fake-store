7379e8aeee16ff70c2e2bf8a57e946d375b21751
JennMomn88 <jenniferpaz88@gmail.com>
2025-03-22 09:39:39 -0400

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import logo from "./../assets/logo-ironhack-blue.png";
-import cart from "./../assets/cart.png";
-import { Link } from "react-router-dom";
+import logo from './../assets/logo-ironhack-blue.png';
+import cart from './../assets/cart.png';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -20,6 +20,7 @@ function Navbar() {
 
         <div className="w-1/4 flex justify-end mr-4">
           {/* User Profile Button */}
+          <Link to={'/Cart'}>
             <button className="flex items-center text-l py-1">
               <img
                 src={cart}
@@ -27,6 +28,7 @@ function Navbar() {
                 className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
               />
             </button>
+          </Link>
         </div>
       </div>
     </nav>
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,17 +1,41 @@
-import { useState } from "react";
-
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function ProductListPage() {
-  // The state variable `products` is currently an empty array [], 
+  // The state variable `products` is currently an empty array [],
   // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => {
+        return response.json();
+      })
 
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+      {products.map((oneProduct) => {
+        return (
+          <Link to={`/product/details/${oneProduct.id}`} key={oneProduct.id}>
+            <div className="card">
+              <img src={oneProduct.image} alt="Product" />
+              <div>{oneProduct.title}</div>
+              <div>{oneProduct.category}</div>
+              <div>{oneProduct.description}</div>
+              <div>{oneProduct.price}</div>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
