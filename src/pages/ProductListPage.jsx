3b4518d2f20bcc7c78dacd1a69e83b884c74ebc3
“Kurt <chaconsaavedrakurt@gmailcomgit config --global user.name Kurt Chacongit config --global user.email chaconsaavedrakurt@gmailcom>
2025-03-23 15:32:49 +0100

    solved lab

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 function ProductListPage() {
@@ -7,11 +7,43 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
-
+useEffect(() => {
+  fetch("https://fakestoreapi.com/products")
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    console.log(data)
+    setProducts(data)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}, [])
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+      {products.map((eachProduct) => {
+        return(
+          <>
+            <Link key ={eachProduct.id} to={`product/details/${eachProduct.id}`}>
+            <div className="ProductList-element">
+             <div style={{ border: "2px solid black", width: "18%", display: "flex", justifyContent: "center" }}>
+              <img src={eachProduct.image} alt="" />
+            </div>
+              <h2>{eachProduct.title}</h2>
+              <p>{eachProduct.category}</p>
+              <p>{peachProduct.price}</p>
+              <p>{eachProduct.description.length > 250 ? eachProduct.description.slice(0, 250) + "..." : eachProduct.description}</p>
+            </div>
+            
+            </Link>
+          
+          
+          </>
+        )
+      })}
     </div>
   );
 }
