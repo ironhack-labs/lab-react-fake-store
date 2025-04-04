1ccd072cb0100177346f28774fb7d52e394d6460
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-04-04 23:32:17 +0200

    initial commit

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
   const [product, setProduct] = useState({});
+
+  const {productId} = useParams();
+
+  async function getProduct (id) {
+    console.log("fetching right product")
+    const response = await fetch(`https://fakestoreapi.com/carts/${id}`)
+    const responseData = await response.json();
+
+    setProduct(responseData)
+  }
+
+  useEffect(() => {
+    getProduct(productId)
+  }, [])
+
+  console.log(productId)
+
 
 
   // The `productId` coming from the URL parameter is available in the URL path.
@@ -18,6 +36,7 @@ function ProductDetailsPage() {
   return (
     <div className="ProductDetailsPage">
     {/* Render product details here */}
+    <h1>Hello</h1>
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {Link} from "react-router-dom"
 
 
 function ProductListPage() {
@@ -7,11 +8,48 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  
+  async function getProducts () {
+    try{
+      const response = await fetch("https://fakestoreapi.com/products");
+      const responseData = await response.json();
+    setProducts(responseData)
+    } catch (err) {
+      console.log(err)
+    }   
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  console.log("prod:", products)
 
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+      {products.map((item) => {
+        return (
+          <>
+            <Link to={`/product/details/${item.id}`}>
+              <div className="product-container" key={item.id}>
+                <img
+                  className="product-img"
+                  src={item.image}
+                  alt={item.title}
+                />
+                <p className="product-title">{item.title}</p>
+                <p className="product-cat">{item.category}</p>
+                <p className="product-price">{item.price}</p>
+                <p className="product-description multi-line-clip">
+                  {item.description}
+                </p>
+              </div>
+            </Link>
+          </>
+        );
+      })}
     </div>
   );
 }
