30d640a09f1179378e0544b34d15ab288fc7cf04
Eliana Depietri <eliana.depietri@gmail.com>
2025-04-30 01:12:51 +0200

    solved lab

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,19 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import ProductListPage from "./pages/ProductListPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
-
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
-
   return (
-    <div className="App relative z-20 pt-20">
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<ProductListPage />} />
-        <Route path="/product/details/:productId" element={<ProductDetailsPage />} />
-      </Routes>
-
-    </div>
+    
+      <div className="App relative z-20 pt-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ProductListPage />} />
+          <Route path="/product/details/:productId" element={<ProductDetailsPage />} />
+        </Routes>
+      </div>
   );
 }
 
