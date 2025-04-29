feb30148aa1831cec70b9b49deccb5fa969f8259
Dreng0t <louischicot4@gmail.com>
2025-04-29 14:49:46 +0100

    solved

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom";
 
 
 function ProductListPage() {
@@ -7,11 +9,37 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    axios
+      .get('/products')
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((e) => console.log('Error getting products from the API...', e));
 
+
+  }, []);
 
   return (
     <div className="ProductListPage">
-      {/* Render list of products here */}
+      <h1>Number of products: {products.length}</h1>
+
+      {products.map((productObj) => (
+        <li key={productObj.id}>
+          <div className="card">
+            <Link to={`/product/details/${productObj.id}`}>
+              <p>Title: {productObj.title}</p>
+              <p>Price: {productObj.price}</p>
+              <p>Category: {productObj.category}</p>
+              <img src={productObj.image} alt="Product image" />
+              <h4>Rating:</h4>
+              <p>Rate: {productObj.rating.rate}</p>
+              <p>Count: {productObj.rating.count}</p>
+
+            </Link>
+          </div>
+        </li>
+      ))}
     </div>
   );
 }
