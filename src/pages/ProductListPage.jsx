b420089ed9967d823ca8238652b4bc05587542c5
Florian Wunsch <Florian95.wunsch@gmail.com>
2025-04-30 00:09:47 +0200

    Done

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,19 +1,55 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function ProductListPage() {
   // The state variable `products` is currently an empty array [], 
   // but you should use it to store the response from the Fake Store API (the list of products).
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(null);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products`)
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => console.log('Error getting characters from the api...', error));
+  }, []);
 
+  if (products === null) {
+    return <p>Loading...</p>;
+  }
 
   return (
-    <div className="ProductListPage">
-      {/* Render list of products here */}
-    </div>
-  );
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+    {products.map((product) => (
+      <Link key={product.id} to={`/product/details/${product.id}`} className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+
+        <img className="w-full" src={product.image} alt={product.title} />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{product.title}</div>
+          <p className="text-gray-700 text-base">
+            {product.description}
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            ${product.price}
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            {product.category}
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            ⭐ {product.rating.rate} ({product.rating.count})
+          </span>
+        </div>
+      </Link>
+    ))}
+  </div>
+);
 }
 
 export default ProductListPage;
+
+
