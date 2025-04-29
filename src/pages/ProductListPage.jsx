70a7030df54e0254c3e61d4f4a16ff208580f7aa
bebopmot <bebopmotinc@gmail.com>
2025-04-29 18:29:49 +0200

    solved lab

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,17 +1,41 @@
-import { useState } from "react";
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useParams } from "react-router-dom"
 
 function ProductListPage() {
-  // The state variable `products` is currently an empty array [], 
-  // but you should use it to store the response from the Fake Store API (the list of products).
+  // State to store the list of products
   const [products, setProducts] = useState([]);
 
-  // To fetch the list of products, set up an effect with the `useEffect` hook:
-
+  // Fetch the products when the component mounts
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log('Error fetching products:', error);
+      });
+  }, []);
 
   return (
-    <div className="ProductListPage">
-      {/* Render list of products here */}
+    <div >
+      <h1>List of products: {products.length}</h1>
+      
+      {products.map((productObj) => {
+        return (
+          <Link to ={`/product/details/${productObj.id}`}>
+          <div className="homepage" key={productObj.id}>
+            <img className = "image" src = {productObj.image}/>
+            <p><strong></strong> {productObj.title}</p>
+            <p><strong>Category:</strong> {productObj.category}</p>
+            <p><strong>Price:</strong> {productObj.price} €</p>
+            <p><strong>Description:</strong> {productObj.description}</p>
+            
+            <hr />
+          </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
