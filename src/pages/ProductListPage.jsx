61965957d061ebe447b4b8d2df153ddfa0b276e4
lynda Sadoudi <lyndaironhack@gmail.com>
2025-05-05 09:04:12 +0200

    done

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
-
+// src/pages/ProductListPage.jsx
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function ProductListPage() {
   // The state variable `products` is currently an empty array [], 
@@ -8,10 +9,28 @@ function ProductListPage() {
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
 
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(err => console.error(err));
+  }, []);
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+      <h1>List of prod</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <Link to={`/product/details/${product.id}`}>
+              <h3>{product.title}</h3>
+              <img src={product.image} alt={product.title} width="100" />
+              <p>{product.price} $</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
